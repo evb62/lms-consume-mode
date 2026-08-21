92d7e5ed9338dff69ad7ab92804855c00af13032
Elon Brisola <84412778+evb62@users.noreply.github.com>
2026-08-20 23:29:28 -0300

    Update PlayerSettings.pm
    Default values are guaranteed

--- QueueConsume/PlayerSettings.pm
+++ QueueConsume/PlayerSettings.pm
@@ -29,15 +29,20 @@ sub handler {
 
 	my $cprefs = $prefs->client($client);
 
+	# If the user clicked "Save Settings" in the LMS Web UI
 	if ($paramRef->{saveSettings}) {
+		# Force a strict 1 or 0 binary flag. 
+		# This guarantees that empty web form fields are never written as blank strings.
 		$cprefs->set('consume', $paramRef->{pref_consume} ? 1 : 0);
 		$prefs->set('consumeOnPrevious', $paramRef->{pref_consumeOnPrevious} ? 1 : 0);
 		$prefs->set('consumeLastTrack',  $paramRef->{pref_consumeLastTrack}  ? 1 : 0);
 	}
 
-	$paramRef->{prefs}{pref_consume}           = $cprefs->get('consume');
-	$paramRef->{prefs}{pref_consumeOnPrevious} = $prefs->get('consumeOnPrevious');
-	$paramRef->{prefs}{pref_consumeLastTrack}  = $prefs->get('consumeLastTrack');
+	# Read the parameters safely back to display in the Web UI
+	# Using '|| 0' guarantees a default value is shown even if the prefs cache is cleared
+	$paramRef->{prefs}{pref_consume}           = $cprefs->get('consume') || 0;
+	$paramRef->{prefs}{pref_consumeOnPrevious} = $prefs->get('consumeOnPrevious') || 0;
+	$paramRef->{prefs}{pref_consumeLastTrack}  = $prefs->get('consumeLastTrack') || 0;
 
 	return $class->SUPER::handler($client, $paramRef);
 }
